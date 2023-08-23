25671b49e07795f29ef2b6c87e00a26f5db58e84
Thalita Rodrigues <112733274+thalitaasuzy@users.noreply.github.com>
2023-08-23 13:41:33 -0300

    Create CalcVelMedia.py

---
+++ CalcVelMedia.py
@@ -0,0 +1,11 @@
+###Velocidade Media###
+###Calcular a Velocidade Media (Vm) de um automóvel que percorreu uma distância (D) em um determinado tempo (T)###
+
+print ("P2 INFO - PHYTON")
+print ("Cálculo de Velocidade Média")
+D = int(input("Informe a Distância (D) em Km: "))
+T = int(input("Informe o tempo (T) em horas: "))
+VelocidadeMedia =  D / T
+print (f"Velocidade Média: {VelocidadeMedia}")
+
+
